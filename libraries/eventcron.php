3f42a210d3d4c36e36e0c6a1c090e5f645dfddc8
Hélder Duarte <cossou@gmail.com>
2013-02-04 23:15:33 +0000

    Update libraries/eventcron.php
    Added flushAllDB and some changes to the log!

--- libraries/eventcron.php
+++ libraries/eventcron.php
@@ -54,7 +54,46 @@ class EventCron extends Event
 				Event::fire($queue->queue, unserialize($queue->arguments)); 	
 				
 				if(Config::get('eventcron::config.log_events'))
-					Log::info('Event ' . $queue->queue . ' was executed!');
+					Log::info('Event [' . $queue->queue . '-' . $queue->id . '] was executed!');
+
+				$queue->processed = true;
+				$queue->ended_at  = (microtime(true) * 10000);
+				$queue->save();
+			} 
+
+			if(Config::get('eventcron::config.log_events'))
+				Log::info('Cronjob ended!');
+
+		}
+		else
+			throw new Exception("This function can only be executed by the CLI (php artisan Eventcron::run queue-name) or just change the configuration file.", 1);
+	}
+	
+	public static function flushAllDB()
+	{
+		if(!Config::get('eventcron::config.enabled'))
+			return;
+
+		if(Config::get('eventcron::config.run_only_from_cli') ? Request::cli() : true)
+		{
+			if(Config::get('eventcron::config.log_events'))
+				Log::info('Cronjob started!');
+
+			$queues = EventCronBase::where_processed(false)
+			->where('date', '<=', date('Y-m-d H:i:s'))
+			->order_by('created_at', 'ASC')
+			->take(Config::get('eventcron::config.max_events_per_execution'))
+			->get();
+
+			foreach ($queues as $queue) 
+			{
+				$queue->started_at = (microtime(true) * 10000);
+				$queue->runned_at  = date('Y-m-d H:i:s');
+
+				Event::fire($queue->queue, unserialize($queue->arguments)); 	
+				
+				if(Config::get('eventcron::config.log_events'))
+					Log::info('Event [' . $queue->queue . '-' . $queue->id . ']  was executed!');
 
 				$queue->processed = true;
 				$queue->ended_at  = (microtime(true) * 10000);
